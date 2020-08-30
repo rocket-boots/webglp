d39e2b81a70c19894599b1ee4034acb6bac80856
Luke <luke.nickerson@gmail.com>
2020-08-30 12:27:14 -0400

    cleanup, jsdoc comments

--- webglp.js
+++ webglp.js
@@ -1,3 +1,5 @@
+// Based on https://xem.github.io/articles/webgl-guide.html
+
 /** STV = Screen Triangle Vertices */
 const STV = new Float32Array([
 	-1, -1, // first triangle
@@ -12,7 +14,13 @@ const STNPV = 2;
 
 const log = (...args) => console.log(...args);
 
+/** Graphic Language Program class */
 class Glp {
+	/**
+	 * Glp constructor method
+	 * @param {Object} gl - webGL object
+	 * @param {Object[]} p - array of webGL program objects
+	 */
 	constructor(gl, p) {
 		Object.assign(this, {
 			gl, // webgl rendering context object
@@ -24,6 +32,10 @@ class Glp {
 			uV: p.map(()=>({})),
 		});
 	}
+	/**
+	 * Use a program
+	 * @param {number} i - index of program
+	 */
 	use(i) {
 		this.i = i;
 		this.gl.useProgram(this.p[i]);
@@ -32,34 +44,50 @@ class Glp {
 	// cp() { // current program
 	// 	return this.p[this.i];
 	// }
-
-	/** Generic setting function -- v = variable storage property, g = get location GL function name, f = set GL function, n = name, a = arguments */
+	/**
+	 * Generic setting function
+	 * @param {string} v - variable storage property
+	 * @param {string} g - get location GL function name
+	 * @param {string} f - set GL function
+	 * @param {string} n - name
+	 * @param {*} a - arguments
+	 */
 	v(v, g, f, n, a) {
 		const o = this;
-		const u = o[v][o.i][n] || o.gl[g](o.p[o.i], n); // get from storage or get location from GL
-		o.gl[`${f}${a.length}f`](u, ...a); // set the values
-		return o[v][this.i][n] = u; // store values and return
+		// Check to see if the value exists in storage already
+		// ...if not then get location from gl with getAttribLocation or getUniformLocation method
+		const d = o[v][o.i][n] || o.gl[g](o.p[o.i], n); // get from storage or get location from GL
+		// Set the value based on the name (u) and the arguments (a), using the gl method
+		// e.g., vertexAttribXf or uniformXf, where X is the length of the arguments
+		o.gl[`${f}${a.length}f`](d, ...a);
+		// Store location id values into this object and return them
+		return o[v][this.i][n] = d;
 	}
-	// An attribute is variable and can contain a float or a vector (vec2, vec3, vec4).
-	// Your program should not exceed 16 attributes to work on all devices.
+	/**
+	 * Set an attribute value
+	 * An attribute is variable and can contain a float or a vector (vec2, vec3, vec4).
+	 * Your program should not exceed 16 attributes to work on all devices.
+	 * @param {string} name 
+	 * @param  {...any} args 
+	 */
 	attr(name, ...args) {
 		return this.v('aV', 'getAttribLocation', 'vertexAttrib', name, args);
-		// const a = this.aV[this.i][name] || this.gl.getAttribLocation(this.p[this.i], name);
-		// this.gl[`vertexAttrib${args.length}f`](a, ...args);
-		// return this.aV[this.i][name] = a;
 	}
-	// A uniform is constant can contain an int, a float, a vector or a matrix (mat2, mat3, mat4).
-	// Your program should not exceed 128 vertex uniforms and 64 fragment uniforms.
+	/**
+	 * Set a uniform value
+	 * A uniform is constant can contain an int, a float, a vector or a matrix (mat2, mat3, mat4).
+	 * Your program should not exceed 128 vertex uniforms and 64 fragment uniforms.
+	 * @param {string} name - the name (aka. location) of the uniform
+	 * @param {...any} args - some number of arguments
+	 */ 
 	unif(name, ...args) {
 		return this.v('uV', 'getUniformLocation', 'uniform', name, args);
-		// const u = this.uV[this.i][name] || this.gl.getUniformLocation(this.p[this.i], name);
-		// this.gl[`uniform${args.length}f`](u, ...args);
-		// return this.uV[this.i][name] = u;
 	}
-	/* Set Uniforms from an array **/
+	/** Set Uniforms from an array */
 	ua(a) {
 		a.forEach(u => this.unif(...u));
 	}
+	/** Set buffer attribute */
 	buff(
 		attr,
 		data,
@@ -96,7 +124,7 @@ class Glp {
 		// this.gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // Clear the canvas AND the depth buffer.
 		this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 	}
-	/** size = how many numbers per vertex */
+	/** Draw/render to the GL content -- vertSize = how many numbers per vertex */
 	draw({
 		uniforms = [],
 		i = this.i,
@@ -112,21 +140,27 @@ class Glp {
 		o.ua(uniforms); // set uniforms
 		o.ba(verts, buffs); // set buffers
 		if (clear) { o.clear(); }
+		// Calculate verts to draw if not defined
 		if (vertsToDraw === undefined) {
 			vertsToDraw = verts.length / vertSize;
 		}
 		o.gl.drawArrays(type, 0, vertsToDraw);
 		return o;
 	}
+	/** Draw all programs (in order) */
 	drawAll(opt = {}) {
 		this.p.forEach((p, i) => this.draw(Object.assign({ i, clear: !i }, opt)));
 	}
 }
 
 const webglp = {
+	/** Glp class */
 	Glp,
+	/** Screen Triangle Vertices */
 	STV,
+	/** Screen Triangles Numbers per vertex */
 	STNPV,
+	/** Get the rendering context from a canvas via the DOM selector */
 	getRenderingContext: (selector, antialias = false) => {
 		const canvas = document.querySelector(selector);
 		const gl = canvas.getContext('webgl', { antialias }); // Get the WebGL rendering context
@@ -135,46 +169,41 @@ const webglp = {
 		}
 		return gl;
 	},
+	/** Load text from a URL using fetch */
 	loadText: (url) => {
 		return fetch(url).then(response => response.text());
 	},
+	/** Load multiple shaders */
 	loadShaders: (urls) => {
 		return Promise.all(urls.map(u => webglp.loadText(u)));
 	},
+	/**
+	 * Compile a webGL shader
+	 * @param {Object} gl - webGL object
+	 * @param {*} type - type of shader, e.g. gl.VERTEX_SHADER
+	 * @param {string} src - source text for shader
+	 */
 	compileShader: (gl, type, src) => {
 		const shader = gl.createShader(type);
 		gl.shaderSource(shader, src);
 		gl.compileShader(shader);
 		return shader;
 	},
+	/** Make the GL canvas match the window's width and height */
 	fullscreen: (gl, win = window) => {
 		gl.canvas.width = win.innerWidth;
 		gl.canvas.height = win.innerHeight;
 		webglp.setViewport(gl);
 	},
+	/** Set the GL viewport to match the canvas width and height */
 	setViewport: (gl) => {
 		gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 	},
-	// Based on https://xem.github.io/articles/webgl-guide.html
-	// TODO: Remove
-	// originalCompile: function (gl, vshader, fshader) {
-	// 	const vs = this.compileShader(gl, gl.VERTEX_SHADER, vshader);
-	// 	const fs = this.compileShader(gl, gl.FRAGMENT_SHADER, fshader);
-	
-	// 	// Create the WebGL program and use it
-	// 	const program = gl.createProgram();
-	// 	gl.aS(program, vs);
-	// 	gl.aS(program, fs);
-	// 	gl.linkProgram(program);
-	// 	gl.useProgram(program);
-	
-	// 	// Log compilation errors, if any
-	// 	log('vertex shader:', gl.getShaderInfoLog(vs) || 'OK');
-	// 	log('fragment shader:', gl.getShaderInfoLog(fs) || 'OK');
-	// 	log('program:', gl.getProgramInfoLog(program) || 'OK');
-
-	// 	return program;
-	// },
+	/**
+	 * Compile a webGL program
+	 * @param {Object} gl - webGL object
+	 * @param {string[]} shaders - array of shader program text
+	 */
 	compile: (gl, shaders) => { // shaders = array of text
 		// Create the WebGL program
 		const program = gl.createProgram();
@@ -194,8 +223,12 @@ const webglp = {
 
 		return program;
 	},
-	// Do it all - Create canvas rendering context, load shaders, compile, and return the context
-	// First param can either be a selector or a GL object
+	/**
+	 * Initialize - Create canvas rendering context, load shaders, compile, and return a Glp object
+	 * @param {string|object} a - A DOM selector string for the canvas, or a webGL object
+	 * @param {String[]} urlsArr - Array of vertex and fragment shader URLs
+	 * @param {Object} options - options, e.g. fullscreen
+	 */
 	init: async (a, urlsArr, { fullscreen } = {}) => {
 		const gl = (typeof a === 'string') ? webglp.getRenderingContext(a) : a;
 		// Do aliases?
